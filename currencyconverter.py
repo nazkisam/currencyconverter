69b87e22964d41bf51feedeaa30a722a82f2f47e
Sameem <nsam603@gmail.com>
2025-02-08 15:07:12 +0530

    api and base URL imported

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,1 +1,9 @@
 import requests
+
+
+API_KEY = 'fca_live_mtW2Qz8bFmihFrf02VsugauY2TSOQpYPVmzAun46'
+BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
+
+
+def converts_currency(base):
+  
